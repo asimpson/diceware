f678d5434097e36c554977adb927ff880a21cc25
Adam Simpson <adam@heysparkbox.com>
2019-10-31 13:36:51 -0400

    feat: add shebang and optional word length via cli

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
+#!/usr/bin/env node
+
 const fetch = require('isomorphic-fetch');
+const numberOfWords = parseInt(process.argv[2], 10) || 5;
 
-const getWordList = () => new Promise((resolve, reject) => {
-  fetch('https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt')
-  .then(x => x.text())
-  .then(x => {
-    const wordObj = x.split('\n').reduce((obj, current) => {
-      const split = current.split('\t');
-      const num = split[0];
-      const word = split[1];
+const getWordList = () =>
+  new Promise((resolve, reject) => {
+    fetch('https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt')
+      .then(x => x.text())
+      .then(x => {
+        const wordObj = x.split('\n').reduce((obj, current) => {
+          const split = current.split('\t');
+          const num = split[0];
+          const word = split[1];
 
-      obj[num] = word;
-      return obj;
-    }, {});
+          obj[num] = word;
+          return obj;
+        }, {});
 
-    resolve(wordObj);
+        resolve(wordObj);
+      });
   });
-});
 
 const rollDice = () => Math.floor(Math.random() * 6) || 1;
 
@@ -24,14 +28,12 @@ const genWord = () => {
   return times.map(() => rollDice()).join('');
 };
 
-const genWords = (length) => new Promise((resolve, reject) => {
-  getWordList().then(list => {
-    const phrase = new Array(length).fill(null).map(() => genWord());
-    resolve(phrase.map(x => list[x]).join(' '));
+const genWords = length =>
+  new Promise((resolve, reject) => {
+    getWordList().then(list => {
+      const phrase = new Array(length).fill(null).map(() => genWord());
+      resolve(phrase.map(x => list[x]).join(' '));
+    });
   });
-});
 
-genWords(5).then(phrase => console.log(phrase));
-
-
-module.exports = genWords;
+genWords(numberOfWords).then(phrase => console.log(phrase));
